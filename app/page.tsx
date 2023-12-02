0c05a69b89cc5e9c00ec9c8f18c06d7423b2f3b4
Joseph Flores <105085111+josephflo@users.noreply.github.com>
2023-12-02 12:13:33 -0300

    feat/api

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
-'use client'
-
+"use client";
+import { FormEvent, useState } from "react";
 import Input from "@/components/Input";
 
 export default function Home() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`;
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
       <div className="bg-white/25 w-full flex flex-col h-fit">
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
           <Input />
-          <h1 className="mb-8 md:0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">Weather App</h1>
+          <h1 className="mb-8 md:0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">
+            Weather App
+          </h1>
         </div>
       </div>
     </div>
