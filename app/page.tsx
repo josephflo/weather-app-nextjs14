f28760d18f358d1b71efaafa82069250e1f44bce
josephflo <josephflo.dev@gmail.com>
2023-12-04 01:01:22 -0300

    feat/input

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,48 @@ export default function Home() {
   const [location, setLocation] = useState("");
   const [error, setError] = useState("");
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=c977ffdd00104ab395e91413230212&q=${location}&days=7&aqi=yes&alerts=yes`;
 
   const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       e.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("City not found");
+        setData({});
+      }
     }
   };
+
+  let content;
+  if (Object.keys(data).length === 0 && error === "") {
+    content = (
+      <div className="text-white text-center">
+        <h2>hola</h2>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen">
       <div className="bg-white/25 w-full flex flex-col h-fit">
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
-          <Input />
+          <Input handleSearch={handleSearch} setLocation={setLocation} />
           <h1 className="mb-8 md:0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">
             Weather App
           </h1>
         </div>
+        {data.current ? (
+          <div>{data.current.temp_f}</div>
+        ): null }
       </div>
     </div>
   );
